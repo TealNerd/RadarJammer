9128e0171182b813bb3b1ad04867576b44725467
ProgrammerDan <programmerdan@gmail.com>
2016-02-02 14:47:32 -0500

    Adding instrumentation

--- src/com/biggestnerd/radarjammer/VisibilityManager.java
+++ src/com/biggestnerd/radarjammer/VisibilityManager.java
@@ -58,15 +58,26 @@ public class VisibilityManager extends BukkitRunnable implements Listener{
 		visThread.start();
 		log.info(String.format("VisibilityManager initialized! minCheck: %d, maxCheck: %d, maxFov: %f, showCombatTagged: %b", minCheck, maxCheck, maxFov, this.showCombatTagged));
 	}
+
+	private long lastCheckRun = 0l;
 	
 	@Override
 	public void run() {
+		long s = System.currentTimeMillis();
+		long b = 0l;
+		long t = 0l;
+		long pl = 0l;
+		long sh = 0l;
+		long hi = 0l;
+		double aqp = 0.0d;
 		for(Player p : Bukkit.getOnlinePlayers()) {
-			
+			b = System.currentTimeMillis();
+			pl++;
 			HashSet<UUID> show = toShow.get(p.getUniqueId());
 			if(show.size() != 0) {
 				for(UUID id : show) {
 					Player o = Bukkit.getPlayer(id);
+					sh++;
 					if(o != null) {
 						log.info(String.format("Showing %s to %s", o.getName(), p.getName()));
 						p.showPlayer(o);
@@ -79,6 +90,7 @@ public class VisibilityManager extends BukkitRunnable implements Listener{
 			if(hide.size() != 0) {
 				for(UUID id : hide) {
 					Player o = Bukkit.getPlayer(id);
+					hi++;
 					if(o != null) {
 						log.info(String.format("Hiding %s from %s", o.getName(), p.getName()));
 						if(showCombatTagged && ctManager.isTagged(id)) continue;
@@ -87,6 +99,15 @@ public class VisibilityManager extends BukkitRunnable implements Listener{
 				}
 			}
 			toHide.get(p.getUniqueId()).clear();
+			t = System.currentTimeMillis();
+			aqp = aqp + ((double)(t-b) - aqp)/pl;
+		}
+		if ((s - lastCheckRun) > 1000l) {
+			if (pl > 0) 
+				log.info(String.format("Updated %d players in %d milliseconds, spending %.2f per player. Total %d seen updates and %d hide updates", pl, (t-s), aqp, sh, hi));
+			else
+				log.info("No players currently tracked.");
+			lastCheckRun = s;
 		}
 	}
 
@@ -122,17 +143,51 @@ public class VisibilityManager extends BukkitRunnable implements Listener{
 			log.info("RadarJammer: Starting calculation thread!");
 			while(true) {
 				delay();
+				lastRun = System.currentTimeMillis();
 				calculate();
+				if (calcRuns > 20) {
+					showAvg();
+				}
 			}
 		}
 
+		private void showAvg() {
+			if (calcRuns == 0d || calcPerPlayerRuns == 0d) {
+				log.info("RadarJammer Calculations Performance: none done.");
+			} else {
+				log.info(String.format("RadarJammer Calculations Performance: %.0f runs, %.0f players calculated for.", calcRuns, calcPerPlayerRuns));
+				log.info(String.format("    on average: %.5fms in setup, %.4fms per player per run, %.4fms per run", (calcSetupAverage / calcRuns), (calcPerPlayerAverage / calcPerPlayerRuns), (calcAverage / calcRuns)));
+			}
+			calcSetupAverage = 0d;
+			calcPerPlayerAverage = 0d;
+			calcPerPlayerRuns = 0d;
+			calcAverage =0d;
+			calcRuns = 0d;
+		}
+
+		private double calcSetupAverage = 0d;
+		private double calcPerPlayerAverage = 0d;
+		private double calcPerPlayerRuns = 0d;
+		private double calcAverage = 0d;
+		private double calcRuns = 0d;
+
 		private void calculate() {
+			calcRuns ++;
+			long s_ = System.currentTimeMillis();
+			long b_ = 0l;
+			long d_ = 0l;
+			long e_ = 0l;
+
 			HashMap<UUID, HashSet<UUID>> checked = new HashMap<UUID, HashSet<UUID>>();
 			HashSet<PlayerLocation> locations = new HashSet<PlayerLocation>();
 			synchronized(lastLocations) {
 				locations.addAll(lastLocations);
 			}
+			e_ = System.currentTimeMillis();
+			calcSetupAverage += (double) (e_ - s_);
 			for(PlayerLocation loc : locations) {
+				calcPerPlayerRuns++;
+				b_ = System.currentTimeMillis();
 				UUID id = loc.getID();
 				if(!checked.containsKey(id)) checked.put(id, new HashSet<UUID>());
 				for(PlayerLocation oloc : locations) {
@@ -175,12 +230,15 @@ public class VisibilityManager extends BukkitRunnable implements Listener{
 					checked.get(id).add(oid);
 					checked.get(oid).add(id);
 				}
+				e_ = System.currentTimeMillis();
+				calcPerPlayerAverage += (e_ - b_);
 			}
+			calcAverage += (e_ - s_);
 		}
 		
 		private void delay() {
 			long time = System.currentTimeMillis() - lastRun;
-			if(time < 100L) {
+			if(time < 100L && time > -1l) {
 				try {
 					sleep(100L - time);
 				} catch (InterruptedException e) {e.printStackTrace();}
