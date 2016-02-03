41c2145d0e827b24be3e04188a18fad218e6b70d
Daniel Boston <programmerdan@gmail.com>
2016-02-02 21:43:17 -0500

    So it goes crazy showing and unshowing, some kind of race condition. Have not sorted it yet. Committing what I have because I need to be doing other things

--- src/com/biggestnerd/radarjammer/VisibilityManager.java
+++ src/com/biggestnerd/radarjammer/VisibilityManager.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
@@ -28,9 +29,12 @@ public class VisibilityManager extends BukkitRunnable implements Listener{
 	private final boolean showCombatTagged;
 	
 	private Set<PlayerLocation> lastLocations;
-	private ConcurrentHashMap<UUID, HashSet<UUID>> hiddenMap;
+	private ConcurrentHashMap<UUID, HashSet<UUID>[]> maps;
+	private AtomicBoolean buffer;
+	
+	/*hidden;
 	private ConcurrentHashMap<UUID, HashSet<UUID>> toShow;
-	private ConcurrentHashMap<UUID, HashSet<UUID>> toHide;
+	private ConcurrentHashMap<UUID, HashSet<UUID>> toHide;*/
 	
 	private VisibilityThread visThread;
 	private TagManager ctManager;
@@ -39,9 +43,12 @@ public class VisibilityManager extends BukkitRunnable implements Listener{
 	public VisibilityManager(RadarJammer plugin, int minCheck, int maxCheck, double maxFov, boolean showCombatTagged) {
 		log = plugin.getLogger();
 		lastLocations = Collections.synchronizedSet(new HashSet<PlayerLocation>());
-		hiddenMap = new ConcurrentHashMap<UUID,	HashSet<UUID>>();
+		maps = new ConcurrentHashMap<UUID, HashSet<UUID>[]>();
+		buffer = new AtomicBoolean();
+
+		/*hiddenMap = new ConcurrentHashMap<UUID,	HashSet<UUID>>();
 		toShow = new ConcurrentHashMap<UUID, HashSet<UUID>>();
-		toHide = new ConcurrentHashMap<UUID, HashSet<UUID>>();
+		toHide = new ConcurrentHashMap<UUID, HashSet<UUID>>();*/
 		this.minCheck = minCheck*minCheck;
 		this.maxCheck = maxCheck*maxCheck;
 		this.maxFov = maxFov;
@@ -61,6 +68,30 @@ public class VisibilityManager extends BukkitRunnable implements Listener{
 
 	private long lastCheckRun = 0l;
 	
+	enum btype {
+		SHOW,
+		HIDE
+	}
+
+	public int getBuffer(btype buf) {
+		if (buffer.get()) {
+			switch(buf) {
+			case SHOW:
+				return 1;
+			case HIDE:
+				return 3;
+			}
+		} else {
+			switch(buf) {
+			case SHOW:
+				return 2;
+			case HIDE:
+				return 4;
+			}
+		}
+		return -1;
+	}
+
 	@Override
 	public void run() {
 		long s = System.currentTimeMillis();
@@ -70,12 +101,27 @@ public class VisibilityManager extends BukkitRunnable implements Listener{
 		long sh = 0l;
 		long hi = 0l;
 		double aqp = 0.0d;
+		boolean buff = false;
+		// Flip buffers.
+		synchronized(buffer) {
+			buff = buffer.get();
+			buffer.set(!buff);
+		}
+
 		for(Player p : Bukkit.getOnlinePlayers()) {
 			b = System.currentTimeMillis();
 			pl++;
-			HashSet<UUID> show = toShow.remove(p.getUniqueId());
-			toShow.putIfAbsent(p.getUniqueId(), new HashSet<UUID>());
-			if(show.size() != 0) {
+			// now get the set of arrays. Careful to only deal with the "locked" ones
+			// based on the semaphore. In this case, "true" gives low-order buffers.
+			UUID pu = p.getUniqueId();
+			HashSet<UUID>[] buffers = maps.get(pu);
+			if (buffers == null) {
+				maps.put(pu, allocate());
+			}
+
+			HashSet<UUID> show = buffers[buff?1:2];//toShow.remove(p.getUniqueId());
+			//toShow.putIfAbsent(p.getUniqueId(), new HashSet<UUID>());
+			if(!show.isEmpty()) {
 				for(UUID id : show) {
 					Player o = Bukkit.getPlayer(id);
 					sh++;
@@ -84,24 +130,30 @@ public class VisibilityManager extends BukkitRunnable implements Listener{
 						p.showPlayer(o);
 					}
 				}
+				show.clear(); // prepare buffer for next swap.
 			}
-			if(p.hasPermission("jammer.bypass")) continue;
-			HashSet<UUID> hide = toHide.remove(p.getUniqueId());
-			toHide.putIfAbsent(p.getUniqueId(), new HashSet<UUID>());
-			if(hide.size() != 0) {
+
+			if (p.hasPermission("jammer.bypass")) continue;
+
+			HashSet<UUID> hide = buffers[buff?3:4];//toHide.remove(p.getUniqueId());
+			//toHide.putIfAbsent(p.getUniqueId(), new HashSet<UUID>());
+			if(!hide.isEmpty()) {
 				for(UUID id : hide) {
 					Player o = Bukkit.getPlayer(id);
 					hi++;
 					if(o != null) {
-						log.info(String.format("Hiding %s from %s", o.getName(), p.getName()));
 						if(showCombatTagged && ctManager.isTagged(id)) continue;
+						log.info(String.format("Hiding %s from %s", o.getName(), p.getName()));
 						p.hidePlayer(o);
 					}
 				}
+				hide.clear();
 			}
+
 			t = System.currentTimeMillis();
 			aqp = aqp + ((double)(t-b) - aqp)/pl;
 		}
+		t = System.currentTimeMillis();
 		if ((s - lastCheckRun) > 1000l) {
 			if (pl > 0) 
 				log.info(String.format("Updated %d players in %d milliseconds, spending %.2f per player. Total %d seen updates and %d hide updates", pl, (t-s), aqp, sh, hi));
@@ -115,18 +167,33 @@ public class VisibilityManager extends BukkitRunnable implements Listener{
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
 		lastLocations.add(new PlayerLocation(player.getEyeLocation(), player.getUniqueId()));
-		hiddenMap.put(player.getUniqueId(), new HashSet<UUID>());
+		HashSet<UUID>[] buffers = maps.get(player.getUniqueId());
+		if (buffers == null) {
+			maps.put(player.getUniqueId(), allocate());
+		} else {
+			for (HashSet<UUID> buffer : buffers){
+				buffer.clear();
+			}
+		}
+		/*hiddenMap.put(player.getUniqueId(), new HashSet<UUID>());
 		toShow.put(player.getUniqueId(), new HashSet<UUID>());
-		toHide.put(player.getUniqueId(), new HashSet<UUID>());
+		toHide.put(player.getUniqueId(), new HashSet<UUID>());*/
+	}
+
+	@SuppressWarnings("unchecked")
+	private HashSet<UUID>[] allocate() {
+		return (HashSet<UUID>[]) new HashSet[] { new HashSet<UUID>(), new HashSet<UUID>(), 
+			new HashSet<UUID>(), new HashSet<UUID>(), new HashSet<UUID>() };
 	}
 	
 	@EventHandler
 	public void onPlayerLeave(PlayerQuitEvent event) {
 		Player player = event.getPlayer();
 		lastLocations.remove(player);
-		hiddenMap.remove(player.getUniqueId());
+		// allocation is expensive. In case they rejoin, leave the data where it was.
+		/*hiddenMap.remove(player.getUniqueId());
 		toShow.remove(player.getUniqueId());
-		toHide.remove(player.getUniqueId());
+		toHide.remove(player.getUniqueId());*/
 	}
 	
 	@EventHandler
@@ -144,6 +211,11 @@ public class VisibilityManager extends BukkitRunnable implements Listener{
 		public void run() {
 			log.info("RadarJammer: Starting calculation thread!");
 			while(true) {
+				// TODO: always updating on every move sounds super expensive. Might be better
+				// to build up a hashSet of players who have moved, then periodically update them 
+				// all en mass.
+				// Probably a good idea to bake in some throttling; LinkedQueues are totally unbounded
+				// so if the rate at which players move >>> the rate of processing, we're screwed.
 				PlayerLocation next = playerQueue.poll();
 				if(next != null) {
 					doCalculations(next);
@@ -185,6 +257,7 @@ public class VisibilityManager extends BukkitRunnable implements Listener{
 			long b_ = 0l;
 			long d_ = 0l;
 			long e_ = 0l;
+			// TODO: next target. This reduction is likely very expensive.
 			HashSet<PlayerLocation> locations = new HashSet<PlayerLocation>();
 			synchronized(lastLocations) {
 				locations.addAll(lastLocations);
@@ -197,6 +270,7 @@ public class VisibilityManager extends BukkitRunnable implements Listener{
 				calcPerPlayerRuns++;
 				b_ = System.currentTimeMillis();
 				UUID oid = other.getID();
+				if (id.equals(oid)) continue;
 				boolean hidePlayer, hideOther;
 				double dist = location.getSquaredDistance(other);
 				if(dist > minCheck) {
@@ -209,24 +283,38 @@ public class VisibilityManager extends BukkitRunnable implements Listener{
 				} else {
 					hidePlayer = hideOther = false;
 				}
-				boolean hidingOther = hiddenMap.get(id).contains(oid);
+				HashSet<UUID>[] buffers = maps.get(id);
+				if (buffers == null) return;
+
+				boolean hidingOther = buffers[0].contains(oid); //hiddenMap.get(id).contains(oid);
 				if(hidingOther != hideOther) {
 					if(hideOther) {
-						toHide.get(id).add(oid);
-						hiddenMap.get(id).add(oid);
+						buffers[getBuffer(btype.HIDE)].add(oid);
+						//toHide.get(id).add(oid);
+						buffers[0].add(oid);
+						//hiddenMap.get(id).add(oid);
 					} else {
-						toShow.get(id).add(oid);
-						hiddenMap.get(id).remove(oid);
+						buffers[getBuffer(btype.SHOW)].add(oid);
+						//toShow.get(id).add(oid);
+						buffers[0].remove(oid);
+						//hiddenMap.get(id).remove(oid);
 					}
 				}
-				boolean hidingPlayer = hiddenMap.get(oid).contains(id);
+
+				buffers = maps.get(oid);
+
+				boolean hidingPlayer = buffers[0].contains(id); //hiddenMap.get(oid).contains(id);
 				if(hidingPlayer != hidePlayer) {
 					if(hidePlayer) {
-						toHide.get(oid).add(id);
-						hiddenMap.get(oid).add(id);
+						buffers[getBuffer(btype.HIDE)].add(id);
+						//toHide.get(oid).add(id);
+						buffers[0].add(id);
+						//hiddenMap.get(oid).add(id);
 					} else {
-						toShow.get(oid).add(id);
-						hiddenMap.get(oid).remove(id);
+						buffers[getBuffer(btype.SHOW)].add(id);
+						//toShow.get(oid).add(id);
+						buffers[0].remove(id);
+						//hiddenMap.get(oid).remove(id);
 					}
 				}
 				e_ = System.currentTimeMillis();
